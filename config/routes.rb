063497631cd189ee19b18f01517c9a911e57b363
kritsada653040438 <Kritsada.ma@kkumail.com>
2024-12-01 13:07:52 +0700

    art_list

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get "/articles", to: "articles#index"
   root "articles#index"
+
+  get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
 end
