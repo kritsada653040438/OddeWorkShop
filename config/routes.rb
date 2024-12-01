928ec2284d24637a1618d8df69071e6162a2277c
kritsada653040438 <Kritsada.ma@kkumail.com>
2024-12-01 13:25:41 +0700

    CURD

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root "articles#index"
-
+  resources :articles
+  
   get "/articles", to: "articles#index"
   get "/articles/:id", to: "articles#show"
 end
