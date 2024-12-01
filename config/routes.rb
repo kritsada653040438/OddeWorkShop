788e9ba393a0fc3c358bef9ba1e168f649685c3d
kritsada653040438 <Kritsada.ma@kkumail.com>
2024-12-01 14:09:10 +0700

    Using Partials to Share View Code & Finishing Up

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   # root "posts#index"
   root "articles#index"
   resources :articles
-  
+
   get "/articles", to: "articles#index"
   get "/articles/:id", to: "articles#show"
 end
